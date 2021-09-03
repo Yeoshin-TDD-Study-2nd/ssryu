dab9d5003656d1f31923c93934edc04c864ea6ff
ssryu87 <ssryu@fastlane.kr>
2021-09-03 15:52:11 +0900

    Initial Commit

---
+++ FortuneCookie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FortuneCookie
+//
+//  Created by ssryu on 2021/09/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
